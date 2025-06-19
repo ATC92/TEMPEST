38964693ab4109a88bfb15a95ac5c17ec1d10a1b
ATC92 <a2201636@uabc.edu.mx>
2025-06-18 18:56:33 -0700

    Update free ArrayTextures

--- src/Game/Game.c
+++ src/Game/Game.c
@@ -119,16 +119,15 @@ void GameInformation(void)
  */
 void DeInitGame(void)
 {
+    for (int i=0; i < 20; i++)
+        UnloadTexture(mapWorld->texturesArray[i]);
+
+        
     free(mapWorld->mapsData);
     free(mapWorld->tileMap);
-    free(mapWorld);
     // mapWorld->texturesArray.
-    // int i =0;
-    // while (mapWorld->texturesArray[i] != NULL)
-    // {
-        
-    // }
     
+    free(mapWorld);
     // FreeHashTable(mapWorld->hashTable);
     ///< Delete Player Data
     free(eplayer->_tileMap);
