0c4543b0e6e732e34cbb3b29ea0ff0507c7c58f6
ATC92 <a2201636@uabc.edu.mx>
2025-09-14 15:25:41 -0700

    Stop tracking build

--- build/Debug/src/Main/CMakeFiles/Project.dir/__/Utils/Information.c.o.d
+++
@@ -1,8 +0,0 @@
-src/Main/CMakeFiles/Project.dir/__/Utils/Information.c.o: \
- /home/atc/Documents/CGame/src/Utils/Information.c \
- /usr/include/stdc-predef.h \
- /home/atc/Documents/CGame/src/Utils/Information.h \
- /home/atc/Documents/CGame/src/Utils/../Components/Mouse.h \
- /home/atc/Documents/CGame/build/Debug/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stdarg.h \
- /usr/lib/gcc/x86_64-linux-gnu/14/include/stdbool.h
